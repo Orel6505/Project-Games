e064d92c5589b9f5634b40f00e30dec0e4fb9ed2
Orel Yosupov <Orely4242@gmail.com>
2025-01-21 11:36:23 +0200

    Project Games: Create action class

---
+++ app/src/main/java/com/orel6505/pgames/action/Action.java
@@ -0,0 +1,14 @@
+package com.orel6505.pgames.action;
+
+public class Action {
+    private String name;
+
+    public Action(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+}
