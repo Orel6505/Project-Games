41dc500efc2f5d195210796851cd46df365dfede
Orel Yosupov <Orely4242@gmail.com>
2025-01-21 10:59:52 +0200

    Project Games: Add increase and getName to Player

--- app/src/main/java/com/orel6505/pgames/player/Player.java
+++ app/src/main/java/com/orel6505/pgames/player/Player.java
@@ -2,7 +2,6 @@ package com.orel6505.pgames.player;
 
 import com.orel6505.pgames.action.Action;
 import com.orel6505.pgames.action.Actionable;
-import com.orel6505.pgames.player.Scorable;
 
 public abstract class Player implements Actionable, Scorable {
     private String name;
@@ -19,11 +18,19 @@ public abstract class Player implements Actionable, Scorable {
         return (this.score > p.getScore());
     }
 
+    public void increaseScore(){
+        updateScore(1);
+    }
+
     public void updateScore(Integer score){
         this.score = this.score + score;
     }
 
     public Integer getScore(){
         return this.score;
+    }
+
+    public String getName(){
+        return this.name;
     }
 }
