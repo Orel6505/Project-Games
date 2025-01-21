4aaac98c2a083a855de3f4fb98240c66eb6ab48f
Orel Yosupov <Orely4242@gmail.com>
2025-01-21 11:35:36 +0200

    Project Games: Implement random player

---
+++ app/src/main/java/com/orel6505/pgames/player/RandomPlayer.java
@@ -0,0 +1,29 @@
+package com.orel6505.pgames.player;
+
+import com.orel6505.pgames.action.Action;
+
+import java.util.Random;
+
+public class RandomPlayer extends Player {
+    private Random rnd;
+
+    public RandomPlayer(String name){
+        super(name);
+        this.rnd = new Random();
+    }
+    
+    @Override
+    public Action selectActions(Action[] actions) {
+        return actions[getRandomNumber(0, actions.length)];
+    }
+
+    private int getRandomNumber(int min, int max) {
+        return this.rnd.nextInt(min,max);
+    }
+
+    @Override
+    public Action selectAction(Action[] actions) {
+        return actions[getRandomNumber(0, actions.length)];
+    }
+
+}
