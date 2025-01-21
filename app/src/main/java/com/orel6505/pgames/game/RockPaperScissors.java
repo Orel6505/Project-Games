6c42bc0595a736f2b7ff1b5a0e860286a85e9c0b
Orel Yosupov <Orely4242@gmail.com>
2025-01-21 12:35:46 +0200

    Project Games:  better spacing for RockPaperScissors action array

--- app/src/main/java/com/orel6505/pgames/game/RockPaperScissors.java
+++ app/src/main/java/com/orel6505/pgames/game/RockPaperScissors.java
@@ -7,7 +7,9 @@ public class RockPaperScissors extends Game {
 
     public RockPaperScissors(Player p1, Player p2){
         super(p1, p2, "Rock Paper Scissors");
-        this.actions = new Action[]{new Action("Rock"), new Action("Paper"), new Action("Scissors")};
+        this.actions = new Action[]{new Action("Rock"), 
+                                    new Action("Paper"), 
+                                    new Action("Scissors")};
     }
 
     @Override
@@ -21,28 +23,18 @@ public class RockPaperScissors extends Game {
 
     @Override
     public void judge(Action a1, Action a2) {
-        if (a1.equals(a2)){
-            return;
+        if (a1.equals(actions[1]) && a2.equals(actions[0])) {
+            p1.increaseScore(); // p1 wins
         }
-        if (a1.equals(actions[0])){
-            if (a2.equals(actions[1])){
-                p2.increaseScore();
-            } else {
-                p1.increaseScore();
-            }
-        } else if (a1.equals(actions[1])){
-            if (a2.equals(actions[0])){
-                p1.increaseScore();
-            } else {
-                p2.increaseScore();
-            }
-        } else {
-            if (a2.equals(actions[0])){
-                p2.increaseScore();
-            } else {
-                p1.increaseScore();
-            }
+        if (a1.equals(actions[2]) && a2.equals(actions[1])) {
+            p1.increaseScore(); // p1 wins
         }
+        if (a1.equals(actions[0]) && a2.equals(actions[1])) {
+            p2.increaseScore(); // p2 wins
+        }
+        if (a1.equals(actions[1]) && a2.equals(actions[2])) {
+            p2.increaseScore(); // p2 wins
+        }
+        //Tie
     }
-    
 }
