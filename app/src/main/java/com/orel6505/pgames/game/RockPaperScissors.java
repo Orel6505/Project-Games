78fe8545f698e280e9644ca454668d400571d394
Orel Yosupov <Orely4242@gmail.com>
2025-01-23 10:25:14 +0200

    Project Games: Update RockPaperScissors based on the changes in Game

--- app/src/main/java/com/orel6505/pgames/game/RockPaperScissors.java
+++ app/src/main/java/com/orel6505/pgames/game/RockPaperScissors.java
@@ -6,7 +6,9 @@ import com.orel6505.pgames.player.Player;
 public class RockPaperScissors extends Game {
 
     public RockPaperScissors(Player p1, Player p2){
-        super(p1, p2, "Rock Paper Scissors");
+        super("Rock Paper Scissors", 2);
+        this.players.add(p1);
+        this.players.add(p2);
         this.actions = new Action[]{new Action("Rock"), 
                                     new Action("Paper"), 
                                     new Action("Scissors")};
@@ -14,17 +16,21 @@ public class RockPaperScissors extends Game {
 
     @Override
     public void play(Integer turnCount){
+        Player p1 = this.players.get(0);
+        Player p2 = this.players.get(1);
         for (int i = 0; i < turnCount; i++){
-            Action a1 = p1.selectAction(actions);
-            Action a2 = p2.selectAction(actions);
-            System.out.println(p1.getName() + " selected " + a1.getName());
-            System.out.println(p2.getName() + " selected " + a2.getName());
-            judge(a1, a2);
+            p1.pushAction(p1.selectAction(actions));
+            p2.pushAction(p2.selectAction(actions));
+            judge(p1, p2);
         }
     }
 
     @Override
-    public void judge(Action a1, Action a2) {
+    protected void judge(Player p1, Player p2) {
+        Action a1 = p1.popAction();
+        Action a2 = p2.popAction();
+        System.out.println(p1.getName() + " selected " + a1.getName());
+        System.out.println(p2.getName() + " selected " + a2.getName());
         if (a1.equals(actions[1]) && a2.equals(actions[0])) {
             p1.increaseScore(); // p1 wins
         }
