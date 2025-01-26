0c3f6087b527859c6bb426714329e0d37992b146
Orel Yosupov <Orely4242@gmail.com>
2025-01-26 11:34:36 +0200

    Project Games: Add a rule in the FireBallSwordMagicRing game the rule that in the FireBallSwordMagicRing the winning player takes the weapon of the loser based on WizardWars rules

--- app/src/main/java/com/orel6505/pgames/game/FireBallSwordMagicRing.java
+++ app/src/main/java/com/orel6505/pgames/game/FireBallSwordMagicRing.java
@@ -18,15 +18,19 @@ public class FireBallSwordMagicRing extends RockPaperScissors {
         System.out.println(p1.getName() + " selected " + a1.getName());
         System.out.println(p2.getName() + " selected " + a2.getName());
         if (a1.equals(actions[1]) && a2.equals(actions[0])) {
+            p1.pushAction(a2);
             p2.setNotActive(); // p1 wins
         }
         if (a1.equals(actions[2]) && a2.equals(actions[1])) {
+            p1.pushAction(a2);
             p2.setNotActive(); // p1 wins
         }
         if (a1.equals(actions[0]) && a2.equals(actions[1])) {
+            p2.pushAction(a1);
             p1.setNotActive(); // p2 wins
         }
         if (a1.equals(actions[1]) && a2.equals(actions[2])) {
+            p2.pushAction(a1);
             p1.setNotActive(); // p2 wins
         }
         //Tie
