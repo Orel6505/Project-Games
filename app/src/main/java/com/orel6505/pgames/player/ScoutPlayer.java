7d2ec75641494e3d6a2d3b4d05707c954fab4ebf
Orel Yosupov <Orely4242@gmail.com>
2025-01-27 22:27:20 +0200

    Project Games: Create ScoutPlayer Definitely not the scout from TF2

---
+++ app/src/main/java/com/orel6505/pgames/player/ScoutPlayer.java
@@ -0,0 +1,33 @@
+package com.orel6505.pgames.player;
+
+import com.orel6505.pgames.action.Action;
+
+public class ScoutPlayer extends Player {
+    private Integer direction;
+    private Action lastAction;
+
+    public ScoutPlayer(String name) {
+        super(name);
+        direction = 0;
+        lastAction = null;
+    }
+
+    @Override
+    public Action selectAction(Action[] actions) {
+        Action currentAction = actions[direction];
+        
+        // If same action was returned last time, it was likely invalid
+        if (currentAction.equals(lastAction)) {
+            direction = (direction + 1) % actions.length;
+            currentAction = actions[direction];
+        }
+        
+        lastAction = currentAction;
+        return currentAction;
+    }
+    
+    @Override
+    public Action selectActions(Action[] actions) {
+        return selectAction(actions);
+    }
+}
